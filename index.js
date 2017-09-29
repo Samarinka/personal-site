871a09d0cf270a0f9500dd9c780b0829a361b7f4
Samarinka <savka.mari@gmail.com>
2017-09-29 15:52:05 +0300

    add gallery to the index

---
+++ index.js
@@ -0,0 +1,26 @@
+$(function(){
+
+	let galleryImage = $("#gallery .gallery").find("img").first();
+	let images =[
+		"img/solia&me.jpg",
+		"img/mountains.jpg",
+		"img/kpi.jpg",
+		"img/cedar.jpg",
+		"img/river.jpg",
+		"img/snail.jpg",
+		"img/umbrellas.jpg"
+
+	];
+
+	let i = 0;
+
+	setInterval(function(){
+		i = (i + 1) % images.length;
+		galleryImage.fadeOut(function(){
+			$(this).attr("src", images[i]);
+			$(this).fadeIn();
+		});
+
+	}, 3000);
+
+});
